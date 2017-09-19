514020e17bd6e13479e45be1be5af57249df47e3
leederacola <rmgforce@gmail.com>
2017-09-19 16:36:32 -0400

    FoodContext: added constructor/connection string set up enticy sets from models, food, users, recipies

--- src/Prototype/Prototype/DAL/FoodContext.cs
+++ src/Prototype/Prototype/DAL/FoodContext.cs
@@ -14,5 +14,30 @@ namespace Prototype.DAL
 {
     public class FoodContext : DbContext
     {
+        /*
+         * Specifying the connection string
+         * The name of the connection string (which you'll add to the Web.config file later) is passed in to the constructor.
+         * If you don't specify a connection string or the name of one explicitly, Entity Framework assumes that the 
+         * connection string name is the same as the class name. The default connection string name in this example 
+         * would then be SchoolContext, the same as what you're specifying explicitly.
+         */
+        public FoodContext() : base("FoodContext")
+        {
+        }
+
+
+        /*
+         * Specifying entity sets
+         * This code creates a DbSet property for each entity set. In Entity Framework terminology, 
+         * an entity set typically corresponds to a database table, and an entity corresponds to a row in the table.
+         */
+        public DbSet<User>Users { get; set; }
+        public DbSet<Food>Foods { get; set; }
+        public DbSet<Recipe>Recipies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
