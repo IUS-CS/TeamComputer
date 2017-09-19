0c40c16d18d15964fc5af2a8dc09b2ebbef5a4b7
leederacola <rmgforce@gmail.com>
2017-09-19 16:29:08 -0400

    Added Data Access Layer Folder  (DAL) created DbContext Class in DAl folder

---
+++ src/Prototype/Prototype/DAL/FoodContext.cs
@@ -0,0 +1,18 @@
+﻿using Prototype.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+
+/*
+ * Create the Database Context
+ * The main class that coordinates Entity Framework functionality for a given data model is the database context class. 
+ * You create this class by deriving from the System.Data.Entity.DbContext class. 
+ * In your code you specify which entities are included in the data model. 
+ * You can also customize certain Entity Framework behavior. In this project, the class is named FoodContextContext.
+ */
+namespace Prototype.DAL
+{
+    public class FoodContext : DbContext
+    {
+    }
+}
