e5368d8a398e473b47f853cdc81aaeebe3cb8c82
leederacola <rmgforce@gmail.com>
2017-09-19 16:23:39 -0400

    nuGet Package manager INstalled Entity Framework. Created model classes in models folder, Food, user, recipe

---
+++ src/Prototype/Prototype/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prototype.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+
+        /*
+         * Navigation properties are typically defined as virtual so that they can take advantage of certain 
+         * Entity Framework functionality such as lazy loading. (Lazy loading will be explained later, in the 
+         * Reading Related Data tutorial later in this series.) If a navigation property can hold multiple 
+         * entities (as in many-to-many or one-to-many relationships), its type must be a list in which entries 
+         * can be added, deleted, and updated, such as ICollection.
+         */
+        public virtual ICollection<Food> Foods { get; set;  }
+    }
+}
