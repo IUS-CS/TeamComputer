1067df288d6fe6b105c2f2336126280572771d90
leederacola <rmgforce@gmail.com>
2017-10-17 16:13:59 -0400

    added Ingredient model

---
+++ src/FoodProject/FoodProject/Models/Ingredient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodProject.Models
+{
+    public class Ingredient
+    {
+        public int IngredientID { get; set; }
+        public int FoodID { get; set; }
+        public int RecipeID { get; set; }
+
+        public virtual Food Food {get; set;}
+        public virtual Recipe Recipe { get; set; }
+    }
+}
