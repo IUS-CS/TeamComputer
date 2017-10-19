cc24060f5fc300b574f4321c46303d61fbc70cae
leederacola <rmgforce@gmail.com>
2017-10-19 16:35:41 -0400

    somethings

--- src/FoodProject/FoodProject/Concrete/FoodContext.cs
+++ src/FoodProject/FoodProject/Concrete/FoodContext.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Data.Entity;
 using FoodProject.Models;
+using FoodProject.Concrete;
 
 namespace FoodProject.Concrete
 {
-    public class FoodContext: DbContext
+    public class FoodContext : DbContext
     {
+        public FoodContext() : base("FoodContext")
+        {
+            Database.SetInitializer<FoodContext>(new CreateDatabaseIfNotExists<FoodContext>());
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Food> Foods { get; set; }
         public DbSet<Pantry> Pantrys { get; set; }
