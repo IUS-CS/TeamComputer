01cf1a92d7ae15074f3ead4877ea8b3cd142c0f9
leederacola <rmgforce@gmail.com>
2017-10-19 14:51:25 -0400

    deleted included view method to start from scratch

--- src/FoodProject/FoodProject/Controllers/RecipeController.cs
+++ src/FoodProject/FoodProject/Controllers/RecipeController.cs
@@ -3,15 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FoodProject.Abstract;
+using FoodProject.Models;
 
 namespace FoodProject.Controllers
 {
     public class RecipeController : Controller
     {
-        // GET: Recipe
-        public ActionResult Index()
-        {
-            return View();
-        }
+
     }
 }
