99b4ae9ebe6c34b120dc6fadf56e298a3bc7c7ea
leederacola <rmgforce@gmail.com>
2017-10-19 15:00:46 -0400

    Created Interface IRecipeRepository

---
+++ src/FoodProject/FoodProject/Abstract/IRecipeRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoodProject.Models;
+
+namespace FoodProject.Abstract
+{
+    public interface IRecipeRepository
+    {
+        IEnumerable<Recipe> Recipes { get; }
+    }
+}
