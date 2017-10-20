db76789e7e15a3179af3c40a8bc9293e000ba454
David <drdcustomlows@gmail.com>
2017-10-20 12:26:49 -0400

    added comments to file

--- src/FoodProject/FoodProject/infrastructure/Binders/UserModelBinder.cs
+++ src/FoodProject/FoodProject/infrastructure/Binders/UserModelBinder.cs
@@ -7,23 +7,30 @@ namespace FoodProject.infrastructure.Binders
 {
     public class UserModelBinder : IModelBinder
     {
+        //ket in the session to get user object
         private const string sessionKey = "User";
 
         public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingConetext)
         {
             User user = null;
+            //checks to see if the session is not null
             if (controllerContext.HttpContext.Session != null)
             {
+                //if not null get the user object out of the session
                 user = (User)controllerContext.HttpContext.Session[sessionKey];
             }
+            //if user == null user in the session object was null
             if (user == null)
             {
+                //need to create a new user object
                 user = new User();
+                //if session is not null add new user object to session
                 if (controllerContext.HttpContext.Session != null)
                 {
                     controllerContext.HttpContext.Session[sessionKey] = user;
                 }
             }
+            //return user object from session or new user object if there wasn't one in the session
             return user;
         }
     }
