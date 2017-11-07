a2729759d6b9c5f67826dccff2ae330c06e2cef3
David <drdcustomlows@gmail.com>
2017-11-07 18:07:50 -0500

    formmated some code

--- src/FoodProject/FoodProject/infrastructure/NinjectDependencyResolver.cs
+++ src/FoodProject/FoodProject/infrastructure/NinjectDependencyResolver.cs
@@ -35,7 +35,7 @@ namespace FoodProject.infrastructure
                 //binds the repositorys to these concrete classes
                 kernel.Bind<IUserRepository>().To<UserRepository>();
                 kernel.Bind<IFoodRepository>().To<FoodRepository>();
-            kernel.Bind<IPantryRepository>().To<PantryRepository>();
+                kernel.Bind<IPantryRepository>().To<PantryRepository>();
             }
 
         }
